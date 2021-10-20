3d4636de7212a32972f17c2d5d978bc75bbcc3ae
Francisco Santos <santos.frau@gmail.com>
2021-10-19 21:19:32 -0300

    Remoção de propriedades não utilizadas na model Apicativo

--- API/Dominio/Model/Aplicativo.cs
+++ API/Dominio/Model/Aplicativo.cs
@@ -2,8 +2,6 @@
 {
     public class Aplicativo
     {
-        public bool Sucesso { get; set; }
-        public string Detalhe { get; set; }
         public long Id { get; set; }
         public string Nome { get; set; }
         public double Preco { get; set; }
